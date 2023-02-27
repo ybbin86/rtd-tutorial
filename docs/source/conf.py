2dddaff0729a123655ad9a6b0506872d107b660f
ybbin86 <hust160251@gmail.com>
2023-02-27 20:57:42 +0900

    add 'myst_parser' extension for using the .md file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,12 @@ author = 'ybeen'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
